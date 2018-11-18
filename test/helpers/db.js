3a6d7f8f94755e7964fc523429e68d2e71230ac2
Vinicius Silva <vsilva472@gmail.com>
2018-11-18 21:42:31 -0200

    include favorite inside db helper

--- test/helpers/db.js
+++ test/helpers/db.js
@@ -7,5 +7,6 @@ exports.clearTables = async () => {
     await models.sequelize.query( 'DELETE FROM `entrycategories`' );
     await models.Category.destroy({ where: {} });
     await models.Entry.destroy({ where: {} });
+    await models.Favorite.destroy({ where: {} });
     await models.User.destroy({ where: {} });
 };
